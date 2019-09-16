1c1a6abd6aed93863889045827e72bc2d6d98202
ariolwork@gmail.com <ariolwork@gmail.com>
2019-09-16 09:11:32 +0300

    method: Only for convex function and linear frames. 	simplex method not realized 	need more examples window: not realized parse of function and frames 	not realized standart derivative function

---
+++ task.py
@@ -0,0 +1,264 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import linprog
+import math
+
+eps = 0.00001
+
+class Func:
+    def __init__(self, func, func_der):
+        self.f = func
+        self.fder = func_der
+        
+    def J_k(self, u_k, u):
+        return sum(self.fder(u_k) * (u - u_k))
+        
+        
+class Frames:
+    #-----------------------------------------------
+    #type 0: frames like a_i <= u_i <= b_i
+    def __set_frames0(self, a, b):
+        self.a = a
+        self.b = b
+    #type 1: frame for symplex method
+    def __set_frames1(self, A, b):
+        self.A = A
+        self.b = b
+        
+    __setframesfuncs = {
+        0: __set_frames0,
+        1: __set_frames1
+    }
+    #-----------------------------------------------
+    def __set_size0(self):
+        if len(self.a) == len(self.b):
+            return len(self.a)
+        else:
+            return -1
+    def __set_size1(self):
+        if len(self.A) == len(self.b):
+            return len(self.b)
+        else:
+            return -1
+        
+    __setSize = {
+        0: __set_size0,
+        1: __set_size1
+    }
+    #------------------------------------------------
+    def __init__(self, type_of_conditions, minimize_func):
+        self.type = type_of_conditions
+        self.minimize = minimize_func
+        self.setframes = self.__setframesfuncs.get(type_of_conditions)
+        self.size = self.__setSize.get(type_of_conditions)
+        
+    def get_size(self):
+        return self.size(self)    
+
+class Job:
+    def __init__(self, func, frames, u_0, alpha):
+        self.f = func
+        self.frames = frames
+        self.u_0 = u_0
+        self.u_k = u_0
+        self.__alpha = alpha
+        self.k = 0
+     
+    def check_errors(self):
+        a = type(self.f.f(self.u_0))
+        b = len(self.f.fder(self.u_0))
+        c = self.frames.get_size()
+        print("func:",a,"\nframes:",c,"\nder:",b,"\nu_0:", len(self.u_0), "\n")
+    
+    def get_next_u(self, u1_k):
+        self.k+=1
+        return self.u_k + self.__alpha(self, u1_k)*(u1_k - self.u_k)
+    
+    def find_u1_k(self):
+        return self.frames.minimize(self.f, self.frames, self.u_k)
+
+class One_variable_function_minimisation_methods:
+    #---------------------------------------------------
+    @staticmethod
+    def bisection_method(func, a, b, eps=0.000001):
+        while True:
+            if b-a < eps:
+                return (a+b)/2
+            sigma = (b-a)/2.0
+            u1 = (a+b-sigma)/2
+            u2 = (a+b+sigma)/2
+            if func(u1)<=func(u2):
+                b = u2
+            else:
+                a = u1
+        return -1
+    #---------------------------------------------------
+    @staticmethod
+    def golden_ratio_method(func, a, b, eps=0.000001):
+        __MAGIC_CONSTANT_1 = (3 - math.sqrt(5))/2
+        __MAGIC_CONSTANT_2 = (math.sqrt(5) - 1)/2
+        while True:
+            if b-a < eps:
+                return (a+b)/2
+            u1 = a + __MAGIC_CONSTANT_1*(b-a)
+            u2 = a + __MAGIC_CONSTANT_2*(b-a)
+            if func(u1)<=func(u2):
+                b = u2
+            else:
+                a = u1
+        return -1
+    #---------------------------------------------------
+    @staticmethod
+    def broken_line_method(func, a, b, L, eps=0.000001):
+        def go(f, u, u0):
+            return f(u0)-L*abs(u-u0)
+        u0 = (a+b)/2
+        p = []
+        while True:
+            u0 = max()
+        return -1
+    #---------------------------------------------------
+    @staticmethod
+    def tangent_method(func, a, b, eps=0.000001):
+        while True:
+            if func.fder(a)>=0:
+                return a
+            if func.fder(b)<=0:
+                return b
+            if abs(a-b)<eps:
+                return (a+b)/2
+            un = (func.f(a)-func.f(b)+b*func.fder(b)-a*func.fder(a))/(func.fder(b)-func.fder(a))
+            if func.fder(un)<0:
+                a = un
+            else:
+                b = un
+
+#print(One_variable_function_minimisation_methods.bisection_method((lambda x: x*x*x-x*x), -5, 1))
+#print(One_variable_function_minimisation_methods.golden_ratio_method((lambda x: x*x*x-x*x), -5, 1))
+#print(One_variable_function_minimisation_methods.tangent_method(Func((lambda x: x*x), (lambda x: 2.0*x)), -1, 2))
+
+
+def alpha_1(J, u1_k):
+    return 1.0/(J.k+1)
+
+def alpha_2(J, u1_k):
+    alpha = 1.0
+    while True:
+        u_k1 = J.u_k + alpha*(u1_k - J.u_k)
+        if J.f.f(u_k1) < J.f.f(J.u_k):
+            return alpha
+        if alpha < eps:
+            break
+        alpha/=2.0
+    return 0.0
+
+def alpha_3(J, u1_k):
+    def f_k(alpha):
+        return J.f.f(J.u_k+alpha*(u1_k-J.u_k))
+    def f_k_der(alpha):
+        return np.dot((u1_k-J.u_k),J.f.fder(J.u_k+alpha*(u1_k-J.u_k)))
+    return One_variable_function_minimisation_methods.tangent_method(Func(f_k, f_k_der), 0, 1)
+
+def frames_minnimize_function(func, frames, u_k):
+    ans = []
+    der = func.fder(u_k)
+    for i in range(0, len(der)):
+        if der[i]>0:
+            ans.append(frames.a[i])
+        elif der[i]<0:
+            ans.append(frames.b[i])
+        else:
+            ans.append((frames.a[i]+frames.b[i])/2)
+    return ans
+
+def symplex_meyhod_minimize_function(func, frames, u_k):
+    return linprog(func.fder(u_k), frames.A, frames.b).x
+
+
+# x^2+xy+y^2
+def f0(u):
+    return u[0]*u[0]+u[0]*u[1]+u[1]*u[1]+0.0
+def f0der(u):
+    return np.array([2*u[0]+u[1], 2*u[1]+u[0]])
+func0 = Func(f0, f0der)
+
+#0<=x<=1, -1<=y<=0
+frames0 = Frames(0, frames_minnimize_function)
+frames0.setframes(frames0, np.array([0.0, -1.0]), np.array([1.0,0.0]))
+#0x+y<=0  0x-y<=1 x+0y<=1 -x+0y<=0
+frames1 = Frames(1, symplex_meyhod_minimize_function)
+frames1.setframes(frames1, np.array([[0,1],[0,-1],[1,0],[-1,0]]), np.array([0,1,1,0]))
+
+
+
+job0 = Job(func0, frames0, np.array([1,-1]), alpha_1) #u0=(1,-1)
+#job0.check_errors()
+job1 = Job(func0, frames0, np.array([-1,0]), alpha_1) #u0=(-1,0)
+#job1.check_errors()
+job2 = Job(func0, frames1, np.array([1, 0]), alpha_1) #u0=(1, 0)
+#job2.check_errors()
+job3 = Job(func0, frames0, np.array([0, 0]), alpha_1) #u0=(0, 0)
+#job3.check_errors()
+
+def calculate_m(job, eps, steps):
+    def method_full(J, eps, steps):
+        f_sequ = []
+        u_k_sequ = []
+        k = 0
+        f_sequ.append(J.f.f(J.u_k))
+        u_k_sequ.append(J.u_k)
+        u_k = 0
+        while True:
+            u1_k = J.find_u1_k()
+            u_k = J.u_k
+            J.u_k = J.get_next_u(u1_k)
+            f_sequ.append(J.f.f(J.u_k))
+            u_k_sequ.append(J.u_k)
+            if k>steps or np.all(J.u_k == u_k) or abs(J.f.f(J.u_k) - J.f.f(u_k)) <= eps:
+                break
+            k+=1
+        return J.f.f(J.u_k), J.u_k, f_sequ, u_k_sequ, k, abs(J.f.f(J.u_k) - J.f.f(u_k))
+    def method_eps(J, eps):
+        f_sequ = []
+        u_k_sequ = []
+        k = 0
+        f_sequ.append(J.f.f(J.u_k))
+        u_k_sequ.append(J.u_k)
+        u_k = 0
+        while True:
+            u1_k = J.find_u1_k()
+            u_k = J.u_k
+            J.u_k = J.get_next_u(u1_k)
+            f_sequ.append(J.f.f(J.u_k))
+            u_k_sequ.append(J.u_k)
+            if k>100000000 or np.all(J.u_k == u_k) or abs(J.f.f(J.u_k) - J.f.f(u_k)) <= eps:
+                break
+            k+=1
+        return J.f.f(J.u_k), J.u_k, f_sequ, u_k_sequ, k, abs(J.f.f(J.u_k) - J.f.f(u_k))
+    def method_steps(J, steps):
+        f_sequ = []
+        u_k_sequ = []
+        k = 0
+        f_sequ.append(J.f.f(J.u_k))
+        u_k_sequ.append(J.u_k)
+        u_k = 0
+#       print("u_k:{}, f:{}".format(J.u_k, J.f.f(J.u_k)))
+        while True:
+            u1_k = J.find_u1_k()
+            u_k = J.u_k
+            J.u_k = J.get_next_u(u1_k)
+ #           print("u_k:{}, f:{}, u1_k:{}".format(J.u_k, J.f.f(J.u_k), u1_k))
+            f_sequ.append(J.f.f(J.u_k))
+            u_k_sequ.append(J.u_k)
+            if k>steps or np.all(J.u_k == u_k):
+                break
+            k+=1
+        return J.f.f(J.u_k), J.u_k, f_sequ, u_k_sequ, k+1, abs(J.f.f(J.u_k) - J.f.f(u_k))
+
+    if steps == -1:
+        return method_eps(job, eps)
+    elif eps == -1:
+        return method_steps(job, steps)
+    return method_full(job, eps, steps)
+
+#calculate_m(job1, -1, 100)
---
+++ window.py
@@ -0,0 +1,174 @@
+from tkinter import *
+from tkinter import messagebox
+import numpy as np
+from tkinter.scrolledtext import ScrolledText
+from task import *
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+alpha1 = "alpha_k = 1/(k+1)\n------------------------\nне гарантирует монотонности"
+alpha2 = "alpha = 1\nwhile J(u_(k+1))>=J(u_k):\n    alpha/=2\n------------------------\nгарантирует монотонность"
+alpha3 = "0<=alpha_k<=1\nf_k(alpha_k) = min(0,1)f_k(a)\nf_k(a)=J(u_k+a(u1_k-u_k))\n------------------------\nметод минизации\nфункции одной переменной"
+
+
+root = Tk()
+root.title("Fuck")
+root.geometry("600x600")
+
+status = 0#0 - примеры 1- ввод своей функции
+alpha_type = IntVar()
+alpha_type.set(1)
+alpha_text = StringVar()
+alpha_text.set(alpha1)
+u0 = StringVar()
+u0.set("0;0.0")
+func_text = StringVar()
+func_text.set("")
+is_steps_frames = IntVar()
+is_steps_frames.set(1)
+steps_frames = StringVar()
+steps_frames.set(10000)
+is_accuracy_frames = IntVar()
+is_accuracy_frames.set(1)
+accuracy_frames = StringVar()
+accuracy_frames.set("0.000001")
+
+
+class Calculate_and_print_ans:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Answer")
+        self.root.geometry("800x400")
+    def calculate(self, func, frames, fder, u_0,  alpha_num, eps, steps):
+        func0 = Func(func, fder)
+        alpha = alpha_1
+        if alpha_num.get() == 2:
+            alpha = alpha_2
+        elif alpha_num.get() == 3:
+            alpha = alpha_3
+        print(alpha_num.get())
+        job = Job(func0, frames0, np.array(list(map(float, (u_0.split(";"))))), alpha) 
+        job.check_errors()
+        print(eps, " ", steps)
+        self.answ = calculate_m(job, float(eps), int(steps))
+    def __get_step_string(self):
+        a = "i|  f  |  u  \n--------------------\n"
+        for i in range(0, len(self.answ[2])):
+            a+="{}| {} | {} \n".format(i, self.answ[2][i], self.answ[3][i])
+        return a
+
+    def draw(self):
+        Label(self.root, text="f:     {}".format(self.answ[0]), fg="#000", wraplength=0, font="Arial 11", padx="10", justify=RIGHT, anchor="nw").place(relx=.0, rely=.05, anchor="sw", relheight=.05, relwidth=.4, bordermode=INSIDE)
+        Label(self.root, text="u:     {}".format(self.answ[1]), fg="#000", wraplength=0, font="Arial 11", padx="10", justify=RIGHT, anchor="nw").place(relx=.0, rely=.1, anchor="sw", relheight=.05, relwidth=.4, bordermode=INSIDE)
+        Label(self.root, text="steps: {}".format(self.answ[4]), fg="#000", wraplength=0, font="Arial 11", padx="10", justify=RIGHT, anchor="nw").place(relx=.0, rely=.15, anchor="sw", relheight=.05, relwidth=.4, bordermode=INSIDE)
+        Label(self.root, text="eps:   {}".format(self.answ[5]), fg="#000", wraplength=0, font="Arial 11", padx="10", justify=RIGHT, anchor="nw").place(relx=.0, rely=.2, anchor="sw", relheight=.05, relwidth=.4, bordermode=INSIDE)
+        st = ScrolledText(self.root)
+        st.place(relx=.4, rely=1, anchor="sw", relheight=1, relwidth=.6, bordermode=INSIDE)
+        st.insert(INSERT, self.__get_step_string())
+        f = Figure(dpi=100)
+        a = f.add_subplot(111)
+        a.plot(self.answ[2])
+        canvas = FigureCanvasTkAgg(f, master=self.root)
+        canvas.draw()
+        canvas.get_tk_widget().place(relx=.0, rely=1, anchor="sw", relheight=.4, relwidth=.4, bordermode=INSIDE)
+#expand=True
+        self.root.mainloop()
+
+
+
+def set_alpha_text():
+    if alpha_type.get() == 1:
+        alpha_text.set(alpha1)
+    elif alpha_type.get() == 2:
+        alpha_text.set(alpha2)
+    else:
+        alpha_text.set(alpha3)
+
+def examples_button_press():
+    global status
+    if status == 0:
+        status = 1
+        hide_examples()
+    else: 
+        status = 0
+        btn_example0.place(relx=.0, rely=.25, anchor="sw", relheight=.25, relwidth=.6, bordermode=INSIDE)
+        btn_example1.place(relx=.0, rely=.5, anchor="sw", relheight=.25, relwidth=.6, bordermode=INSIDE)
+def hide_examples():
+    btn_example0.place_forget()
+    btn_example1.place_forget()
+def example_0_press():
+    stps = "-1"
+    if is_steps_frames.get() == 1:
+        stps = steps_frames.get()
+    acc = "-1"
+    if is_accuracy_frames.get() == 1:
+        acc = accuracy_frames.get()
+    c1 = Calculate_and_print_ans()
+    c1.calculate(f0, frames0 ,f0der, u0.get(), alpha_type, acc, stps)
+    c1.draw()
+def example_1_press():
+    stps = "-1"
+    if is_steps_frames.get() == 1:
+        stps = steps_frames.get()
+    acc = "-1"
+    if is_accuracy_frames.get() == 1:
+        acc = accuracy_frames.get()
+    c1 = Calculate_and_print_ans()
+    c1.calculate(f0, frames1 ,f0der, u0.get(), alpha_type, acc, stps)
+    c1.draw()
+
+def my_func_button_press():
+    global status
+    status = 1
+
+def calculate():
+    return 1
+
+
+btn0 = Button(text="examples", background="#ccc", foreground="#000", activebackground="#ccc",
+             padx="10", pady="4", font="Arial 14", width="20", command=examples_button_press)
+btn0.place(relx=.8, rely=.05, anchor="sw", relheight=.05, relwidth=.195, bordermode=INSIDE)
+#btn1 = Button(text="own", background="#ccc", foreground="#000", activebackground="#ccc",
+#             padx="10", pady="4", font="Arial 14", width="20", command=my_func_button_press)
+#btn1.place(relx=.8, rely=.1, anchor="sw", relheight=.05, relwidth=.195, bordermode=INSIDE)
+btn2 = Button(text="calculate", background="#ccc", foreground="#000", activebackground="#ccc",
+             padx="10", pady="4", font="Arial 14", width="20", command=calculate)
+btn2.place(relx=.6, rely=.99, anchor="sw", relheight=.05, relwidth=.395, bordermode=INSIDE)
+
+
+rb1 = Radiobutton(text="Alpha 1", value=1, variable=alpha_type, padx=10, pady=4, font="Arial 14", command=set_alpha_text)
+rb1.place(relx=.6, rely=.05, anchor="sw", relheight=.05, relwidth=.2, bordermode=INSIDE)
+Radiobutton(text="Alpha 2", value=2, variable=alpha_type, padx=10, pady=4, font="Arial 14", command=set_alpha_text).place(relx=.6, rely=.1, anchor="sw", relheight=.05, relwidth=.2, bordermode=INSIDE)
+Radiobutton(text="Alpha 3", value=3, variable=alpha_type, padx=10, pady=4, font="Arial 14", command=set_alpha_text).place(relx=.6, rely=.15, anchor="sw", relheight=.05, relwidth=.2, bordermode=INSIDE)
+Label(textvariable=alpha_text, fg="#000", bg="#bbb", wraplength=0, font="Arial 12", padx="10", pady="4", justify=LEFT, anchor="nw", bd=.05).place(relx=.6, rely=.89, anchor="sw", relheight=.74, relwidth=.395, bordermode=INSIDE)
+Label(text="u0:", fg="#000", wraplength=0, font="Arial 12", padx="10", pady="4", justify=RIGHT, anchor="ne").place(relx=.6, rely=.94, anchor="sw", relheight=.05, relwidth=.2, bordermode=INSIDE)
+Entry(textvariable=u0).place(relx=.8, rely=.94, anchor="sw", relheight=.05, relwidth=.195, bordermode=INSIDE)
+
+Label(text="f:", fg="#000", wraplength=0, font="Arial 12", padx="10", pady="4", justify=RIGHT, anchor="ne").place(relx=.0, rely=.05, anchor="sw", relheight=.05, relwidth=.1, bordermode=INSIDE)
+Entry(textvariable=func_text).place(relx=.1, rely=.05, anchor="sw", relheight=.05, relwidth=.4, bordermode=INSIDE)
+Label(text="frames:", fg="#000", wraplength=0, font="Arial 12", padx="10", pady="4").place(relx=.0, rely=.1, anchor="sw", relheight=.05, relwidth=.6, bordermode=INSIDE)
+ScrolledText().place(relx=.1, rely=.4, anchor="sw", relheight=.3, relwidth=.4, bordermode=INSIDE)
+
+Checkbutton(text="Steps", variable=is_steps_frames, onvalue=1, offvalue=0).place(relx=.0, rely=.95, anchor="sw", relheight=.05, relwidth=.1, bordermode=INSIDE)
+Entry(textvariable=steps_frames).place(relx=.1, rely=.95, anchor="sw", relheight=.05, relwidth=.4, bordermode=INSIDE)
+
+Checkbutton(text="Accur", variable=is_accuracy_frames, onvalue=1, offvalue=0).place(relx=.0, rely=1, anchor="sw", relheight=.05, relwidth=.1, bordermode=INSIDE)
+Entry(textvariable=accuracy_frames).place(relx=.1, rely=1, anchor="sw", relheight=.05, relwidth=.4, bordermode=INSIDE)
+
+btn_example0_text = "f(x,y)=x^2+xy+y^2\n0<=x<=1\n-1<=y<=0"
+btn_example0 = Button(text=btn_example0_text, background="#ccc", foreground="#000", activebackground="#ccc",
+             padx="10", pady="4", font="Arial 14", width="20", command=example_0_press)
+btn_example1_text = "f(x,y)=x^2+xy+y^2\n0x+y<=0\n0x-y<=1\nx+0y<=1\n-x+0y<=0"
+btn_example1 = Button(text=btn_example1_text, background="#ccc", foreground="#000", activebackground="#ccc",
+             padx="10", pady="4", font="Arial 14", width="20", command=example_1_press)
+btn_example0.place(relx=.0, rely=.25, anchor="sw", relheight=.25, relwidth=.6, bordermode=INSIDE)
+btn_example1.place(relx=.0, rely=.5, anchor="sw", relheight=.25, relwidth=.6, bordermode=INSIDE)
+#btn_example0.place_forget()
+
+#def show_message():
+#    messagebox.showinfo("GUI Python", message.get())#
+
+root.mainloop()
+
